18196185b3b5cb07d434218665e272a2730a5c60
ws7solution-hue <ws7solution@gmail.com>
2026-07-02 02:54:31 -0300

    Update mp-create-preference.js

--- api/mp-create-preference.js
+++ api/mp-create-preference.js
@@ -7,8 +7,8 @@ const MP_ACCESS_TOKEN = process.env.MP_ACCESS_TOKEN || "";
 const PLAN_PRICES = {
   starter: 397,
   profissional: 597,
-  clinica: 997,
-  premium: 0.03,
+  clinica: 0.02,
+  premium: 1497,
 };
 
 const PLAN_NAMES = {
